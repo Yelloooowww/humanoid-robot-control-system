ea1de7af5e48bd182b1714c3aee28b6cc6c89667
Yellow <cococing0324@gmail.com>
2018-11-17 19:06:46 +0800

    學習mode ,總表相關功能完成 人機已更新可以用人機測試

--- library/part_of_New_Outlook.c
+++ library/part_of_New_Outlook.c
@@ -1,36 +1,41 @@
-//模擬程式:學習mode完成測試成功
-//藍牙改過baudrate(115200)
+//模擬程式:學習mode 總表紀錄,清空,刪除 完成 人機已更新測試正常
 #include "ASA_Lib.h"
+#include <avr/interrupt.h>
+#include <math.h>
+#include <string.h>
 #define FOSC 11059200// Clock Speed
-#define BAUD1 115200
+#define BAUD1 9600//(暫時改回來)
 #define MYUBRR1 (FOSC/16/BAUD1-1)
-volatile uint8_t i,j,get[200];
+volatile uint8_t get[3];
 
 struct FIFO_for_angle{
-  uint8_t index_start;
-  uint8_t index_end;
+  uint8_t index_start;//存到哪
+  uint8_t index_end;//處理到哪
   uint8_t container[10][2];
 }angle_FIFO={-1,-1,{{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}}};
-
+typedef enum {Header,Bytes,type,Data,checksum}state;
+state now_state;
 
 uint8_t mode;//輪循(mode=0:學習,mode=1:控制)
 uint8_t command;//伺服機設定命令
 uint8_t now[17];//姿態FIFO
-typedef enum {Header,Bytes,type,Data,checksum}state;
-state now_state;
+uint8_t accumulate[200];//準備寫入SDC的資料(和人機上的總表大致相同)(寫入SDC的過程要內插)
+//範例:accumulate[]={36,75,75,75,75,75,75,75,75,75,75,75,75,75,75,75,75,75,3,76,75,75,75,75,75,75,75,75,75,75,75,75,75,75,75,75,255}
+//說明: 36:資料長度(18*2);  75.....:該時刻17軸角度資料; 3:和下一時刻資料要內插成3個間格 ;255:資料結束(沒有下一時刻了)
+
 
 void messenger_dealer(){     //訊息交換機MCU
-  if(get[0]!=0 && get[1]!=0 && get[2]==0){
+  if((get[0]|get[1])!=0 && get[2]==0){
     mode=0;//進入學習模式
     while ((angle_FIFO.index_start+1)%10 == angle_FIFO.index_end) {
       printf("FIFO is FULL\n" );
     }
     //存入FIFO
-    printf("ADD to FIFO\n" );
+    // printf("ADD to FIFO\n" );
     angle_FIFO.index_start=(angle_FIFO.index_start+1)%10;
     angle_FIFO.container[0][angle_FIFO.index_start]=get[0];
     angle_FIFO.container[1][angle_FIFO.index_start]=get[1];
-  }else if(get[0]==0 && get[1]==0 && get[2]!=0){
+  }else if((get[0]|get[1])==0 && get[2]!=0){
     mode=1;//進入控制模式
     command=get[2];
     printf("ENTER Control Mode\n" );
@@ -103,7 +108,7 @@ void USART_Init( unsigned int ubrr )
 {
   UBRR1H |= (unsigned char)(ubrr>>8);
   UBRR1L |= (unsigned char)ubrr;
-  UCSR1B|=(1<<RXCIE1)|(1<<TXCIE1);//Tx Rx中斷致能
+  UCSR1B|=(1<<RXCIE1);//Rx中斷致能
   UCSR1B |= (1<<RXEN1)|(1<<TXEN1);//Tx Rx致能
   UCSR1B &= (~(1<<UCSZ12));//8-bit: UCSZn2=0,UCSZn1=1,UCSZn0=1
   UCSR1C |=(1<<UCSZ11)|(1<<UCSZ10);
@@ -113,11 +118,9 @@ void USART_Init( unsigned int ubrr )
 
 void robot_gesture_player(){//機器人姿態播放器
   if(mode==0){
-    if(angle_FIFO.index_end == angle_FIFO.index_start){
-      printf("FIFO Is Gone\n" );
-    }else{
+    while(angle_FIFO.index_end != angle_FIFO.index_start){    //一直處理直到FIFO變成空的
       angle_FIFO.index_end = (angle_FIFO.index_end+1)%10;
-      printf("index_end = %d\n", angle_FIFO.index_end);
+      // printf("index_end = %d\n", angle_FIFO.index_end);
       uint8_t ID=angle_FIFO.container[0][angle_FIFO.index_end];
       uint8_t a=angle_FIFO.container[1][angle_FIFO.index_end];
       printf("ID=%d to angle=%d\n",ID,a );
@@ -126,12 +129,145 @@ void robot_gesture_player(){//機器人姿態播放器
       for(int i=0;i<17;i++) printf("%d ;",now[i]);
       printf("\n" );
     }
+    // printf("FIFO Is Gone\n");
   }else{
     ;//處理SDC_FIFO(動作姿態串暫存器)裡的東西,更新now[](姿態)
   }
 }
+void ACK( unsigned char data ) //單純的UART1送訊
+{
+  UDR1 = data;
+  while ( !( UCSR1A & (1<<UDRE1)) )  //If UDREn is one, the buffer is empty
+  ;
+}
+void Update_accmulate(uint8_t c){  //更新總表
+  if(c<=6 && c>=1){ //使用者按紀錄
+    if(c<=5 && c>=1) accumulate[accumulate[0]]=c;//間格
+    for(int i=0;i<17;i++){
+      accumulate[accumulate[0]+1+i]=now[i];
+    }
+    accumulate[accumulate[0]+18]=255;//資料結尾
+    accumulate[0]+=18;
+    printf("Have Recorded\n");
+  }else if(c==7){//按DEL 清除前一時刻資料
+    accumulate[0]-=18;
+    for(int i=0;i<=17;i++){
+      accumulate[accumulate[0]+1+i]=0;
+    }
+    accumulate[accumulate[0]]=255;//資料結尾
+    printf("Have DEL\n");
+  }else if(c==8){//按清空
+    uint8_t tmp=accumulate[0];
+    for(int i=0;i<=tmp;i++) accumulate[i]=0;
+  }
+  for(int i=0; i<60 ;i++) printf("acc[%d]=%d\n",i,accumulate[i] );
+}
+
+void command_processor(uint8_t c){//監控命令處理器
+  if(c!=0){//伺服機設定命令不是空的
+    if(c<=9 && c>=1){//人機相關命令
+      if(c>=1 && c<=8){
+        Update_accmulate(c); //更新總表
+        command=0;//處理完命令把command歸零
+      }else if(c==9){//按試播
+        ;
+      }
 
 
+
+
+    }else if(c<=18 && c>=11){//SDC寫入相關命令
+      switch (c) {
+        case 11:
+            printf("Save To SDC(1)\n");
+            ACK(132);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 12:
+            printf("Save To SDC(2)\n");
+            ACK(132);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 13:
+            printf("Save To SDC(3)\n");
+            ACK(132);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 14:
+            printf("Save To SDC(4)\n");
+            ACK(132);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 15:
+            printf("Save To SDC(5)\n");
+            ACK(132);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 16:
+            printf("Save To SDC(6)\n");
+            ACK(132);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 17:
+            printf("Save To SDC(7)\n");
+            ACK(132);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 18:
+            printf("Save To SDC(8)\n");
+            ACK(132);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        }
+
+
+
+    }else if(c<=28 && c>=21){//SDC資料播放相關命令
+      switch (c) {
+        case 21:
+            printf("Action 1\n");
+            ACK(131);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 22:
+            printf("Action 2\n");
+            ACK(131);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 23:
+            printf("Action 3\n");
+            ACK(131);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 24:
+            printf("Action 4\n");
+            ACK(131);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 55:
+            printf("Action 5\n");
+            ACK(131);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 26:
+            printf("Action 6\n");
+            ACK(131);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 27:
+            printf("Action 7\n");
+            ACK(131);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+        case 28:
+            printf("Action 8\n");
+            ACK(131);//模擬一下可以ack的情形
+            command=0;//處理完命令把command歸零
+            break;
+      }
+    }
+  }
+}
 
 int main(){
   ASA_M128_set();
@@ -147,8 +283,7 @@ int main(){
         break;
       }
       case 1:{//控制模式
-        printf("Control Mode\n" );
-        // command_processor(command);//監控命令處理器
+        command_processor(command);//監控命令處理器
         break;
       }
     }
@@ -161,64 +296,3 @@ ISR(USART1_RX_vect) {
 	uint8_t g=UDR1;
   my_decoder(g);
 }
-ISR(USART1_TX_vect) {
-	j++;
-}
-
-
-
-
-
-
-
-
-
-
-
-/*    暫用測試python程式 人機程式尚未更新
-import sys
-import time
-import serial
-
-a1=[3,75,0]
-a2=[3,76,0]
-a3=[3,77,0]
-a4=[3,78,0]
-a5=[3,79,0]
-
-def what_is_type(data):
-    return 1
-def encoder_and_send_pac(data):
-    pac=[]
-    pac +=  [253]  #Header=0xfd
-    typeeeee=what_is_type(data)
-    pac+=bytes([typeeeee]) #type
-    l=bytes([len(data)>>8,len(data)&0xff])
-    pac+= l  #bytes(2bytes)
-    pac += data  #data
-    checksum= bytes([(sum(data)+sum(l))&0xFF])
-    pac += checksum #checksum
-    for x in pac:
-        ser.write(bytes([x]))
-        print('pac=',x)
-        # time.sleep(0.5)
-
-
-
-if __name__ == "__main__":
-    ser = serial.Serial()
-    ser.baudrate = 115200
-    ser.timeout=1000
-    ser.port = str('COM10')
-    ser.open()
-    time.sleep(5)
-    print('HaveOpen')
-    while 1:
-        encoder_and_send_pac(a1)
-        encoder_and_send_pac(a2)
-        encoder_and_send_pac(a3)
-        encoder_and_send_pac(a4)
-        encoder_and_send_pac(a5)
-    print('Done')
-
-*/
