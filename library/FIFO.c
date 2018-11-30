e65bdf12b862ef137e103ca602e51d4979ae0d13
Yellow <cococing0324@gmail.com>
2018-12-01 00:56:07 +0800

    用Putin和Takeout函式操作我們的共用FIFO 注意一定要 uint16_t 陣列長度17

--- library/FIFO.c
+++ library/FIFO.c
@@ -1,11 +1,12 @@
+//上次忘記我們的資料長度應該用uint16_t了
 #include "ASA_Lib.h"
 
 unsigned int  SDC_FIFO_max=10;//for FIFO
 unsigned int  SDC_FIFO_rear=9;//for FIFO
 unsigned int  SDC_FIFO_front=9;//for FIFO
-uint8_t KONDO_SDC_FIFO[10][17];//for FIFO，需小於最小檔案之資料量
+uint16_t KONDO_SDC_FIFO[10][17];//for FIFO，需小於最小檔案之資料量
 
-void PutIn(uint8_t *p){
+void PutIn(uint16_t *p){
   if((SDC_FIFO_rear+1)%SDC_FIFO_max==SDC_FIFO_front){
     printf("FIFO Is FULL\n" );
   }else{
@@ -18,7 +19,7 @@ void PutIn(uint8_t *p){
   printf("AfterPut:SDC_FIFO_rear=%d SDC_FIFO_front=%d\n", SDC_FIFO_rear,SDC_FIFO_front);
 }//void PutIn(uint8_t *p)
 
-void TakeOut(uint8_t *t){
+void TakeOut(uint16_t *t){
   if(SDC_FIFO_front == SDC_FIFO_rear){
     printf("FIFO Is Empty\n");
   }else{
@@ -36,10 +37,10 @@ int main(){
   int i=1;
   printf("START~~~~~~~~~~~~~~~~~~~~~~~~\n" );
   while (i++) {
-    uint8_t put_into_FIFO[17]={i,i+1,i+2,i+3,i+4,i+5,i+6,i+7,i+8,i+9,i+10,i+11,i+12,i+13,i+14,i+15,i+16};
+    uint16_t put_into_FIFO[17]={i+1000,i+1001,i+1002,i+1003,i+1004,i+1005,i+1006,i+1007,i+1008,i+1009,i+1010,i+1011,i+1012,i+1013,i+1014,i+1015,i+1016};//一定要uint16_t!!!
     PutIn(put_into_FIFO);
 
-    uint8_t take_out_from_FIFO[17];
+    uint16_t take_out_from_FIFO[17];  //一定要uint16_t!!!
     TakeOut(take_out_from_FIFO);
     printf("take_out_from_FIFO=");
     for(int i=0;i<17;i++){
@@ -47,41 +48,6 @@ int main(){
     }
     printf("\n" );
   }
-  // while (i++) {
-  //   // put in
-  //     uint8_t put_into_FIFO[17]={i,i+1,i+2,i+3,i+4,i+5,i+6,i+7,i+8,i+9,i+10,i+11,i+12,i+13,i+14,i+15,i+16};
-  //     if((SDC_FIFO_rear+1)%SDC_FIFO_max==SDC_FIFO_front){
-  //       printf("FIFO Is FULL\n" );
-  //     }else{
-  //       printf("put in\n" );
-  //       SDC_FIFO_rear=(SDC_FIFO_rear+1)%SDC_FIFO_max;
-  //       for(int i=0;i<17;i++){
-  //         KONDO_SDC_FIFO[i][SDC_FIFO_rear]=put_into_FIFO[i];
-  //       }
-  //     }
-  //     printf("AfterPut:SDC_FIFO_rear=%d SDC_FIFO_front=%d\n", SDC_FIFO_rear,SDC_FIFO_front);
-
-    // //take out
-    //   uint8_t take_out_from_FIFO[17];
-    //   if(SDC_FIFO_front == SDC_FIFO_rear){
-    //     printf("FIFO Is Empty\n");
-    //   }else{
-    //     printf("take out\n" );
-    //     SDC_FIFO_front=(SDC_FIFO_front+1)%SDC_FIFO_max;
-    //     for(int i=0;i<17;i++){
-    //       take_out_from_FIFO[i]=KONDO_SDC_FIFO[i][SDC_FIFO_front];
-    //     }
-    //   }
-    //   printf("AfterTake:SDC_FIFO_rear=%d SDC_FIFO_front=%d\n", SDC_FIFO_rear,SDC_FIFO_front);
-    //
-    //   printf("take_out_from_FIFO=");
-    //   for(int i=0;i<17;i++){
-    //     printf("%d ",  take_out_from_FIFO[i] );
-    //   }
-    //   printf("\n" );
-  }
-
-
 
 
   return 0;
